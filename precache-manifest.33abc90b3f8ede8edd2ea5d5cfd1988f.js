db3b853edd3abd5007452e44174de4bcc39b6b9a
atumv <atumv.gm@gmail.com>
2021-03-30 01:57:54 +0700

    Updates

---
+++ precache-manifest.33abc90b3f8ede8edd2ea5d5cfd1988f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be27cccf9fb3961d67aa17ee8531c4c",
+    "url": "/todo-redux/index.html"
+  },
+  {
+    "revision": "caac6581b82111156b58",
+    "url": "/todo-redux/static/css/main.7e697d0e.chunk.css"
+  },
+  {
+    "revision": "6eefe1235fb3c0f4d8ac",
+    "url": "/todo-redux/static/js/2.e2995de1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-redux/static/js/2.e2995de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caac6581b82111156b58",
+    "url": "/todo-redux/static/js/main.f8e771cc.chunk.js"
+  },
+  {
+    "revision": "aa652c6dc51e20939694",
+    "url": "/todo-redux/static/js/runtime-main.bdcb7bf2.js"
+  }
+]);
